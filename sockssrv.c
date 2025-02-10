97fbf8d2d0eafef968c43ce25c745877ece56f09
Viacheslav Chimishuk <vchimishuk@yandex.ru>
2025-02-10 14:56:59 +0200

    Increase stack size to 32K on OpenBSD/Clang (#84)
    microsocks segfaults on OpenBSD when building with Clang. However, building
    it with -O0 make the issue gone with default 16K stack. GCC works good under
    any conditions.
    32K stack works good on OpenBSD -current (7.6).

--- sockssrv.c
+++ sockssrv.c
@@ -59,6 +59,9 @@
 #elif defined(__GLIBC__) || defined(__FreeBSD__) || defined(__sun__)
 #undef THREAD_STACK_SIZE
 #define THREAD_STACK_SIZE 32*1024
+#elif defined(__OpenBSD__) && defined(__clang__)
+#undef THREAD_STACK_SIZE
+#define THREAD_STACK_SIZE 32*1024
 #endif
 
 static int quiet;
